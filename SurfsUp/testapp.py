f3a20aad1be4c71fca5790425edc07f546161642
Galen <gmitt98@users.noreply.github.com>
2023-05-11 11:44:21 -0700

    update main

--- SurfsUp/testapp.py
+++ SurfsUp/testapp.py
@@ -16,8 +16,8 @@ cwd = os.getcwd()
 parent_dir = os.path.abspath(os.path.join(cwd, os.pardir))
 print(parent_dir)
 dbpath = f"{parent_dir}/sqlalchemy-challenge/Resources/hawaii.sqlite" # for some reason i had to add one more folder level
-engine = create_engine(f"sqlite:///{dbpath}")
 print(dbpath)
+engine = create_engine(f"sqlite:///{dbpath}")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -121,7 +121,7 @@ def get_stations3():
 
 
 @app.route('/api/v1.0/tobs')
-def tobs():
+def get_tobs():
     session = Session(engine)
     max_date_query = session.execute('SELECT MAX(date) FROM measurement')
     for row in max_date_query:
@@ -133,23 +133,41 @@ def tobs():
     for row in station_activity_result:
         station_activity_result_list.append(row)
     most_active_station_name = station_activity_result_list[0][0]
-    query = 'select date, tobs from measurement where station = :most_active_station_name and date > :one_year_ago'
+    query = 'select date, tobs from measurement where station = :most_active_station_name and date >= :one_year_ago'
     most_active_data_query_hist = session.execute(query, {'most_active_station_name':most_active_station_name, 'one_year_ago':one_year_ago})
-    most_active_data_dict = [] # I will write the data to a dictionary
+    most_active_data_dict_list = [] # I will write the data to a dictionary
     for row in most_active_data_query_hist:
-        most_active_data_dict.append({'date': row[0], 'tobs': row[1]})
-    return jsonify(most_active_data_dict)
+        most_active_data_dict_list.append({'date': row[0], 'tobs': row[1]})
+    return jsonify(most_active_data_dict_list)
 
-@app.route('/api/v1.0/<start>')
-def hello6():
-    return('hello6')
+@app.route('/api/v1.0/<start>') # <start> is the variable part of the route url
+def get_start(start):
+    session = Session(engine)
+    query = 'SELECT MIN(tobs), AVG(tobs), MAX(tobs) FROM measurement WHERE date >= :start'
+    start_stats = session.execute(query, {'start':start})
+    start_stats_dict_list = []
+    for min_temp, avg_temp, max_temp in start_stats:
+        start_stats_dict = {}
+        start_stats_dict['min_temp'] = min_temp
+        start_stats_dict['avg_temp'] = round(avg_temp,1)
+        start_stats_dict['max_temp'] = max_temp
+        start_stats_dict_list.append(start_stats_dict)
+    return jsonify(start_stats_dict_list)
 
-@app.route('/api/v1.0/<start>/<end>')
-def hello7():
-    return 'Hello, World!'
-
+@app.route('/api/v1.0/<start>/<end>') # <start> is the first variable of the route url and <end> is the second
+def get_start_end(start,end):
+    session = Session(engine)
+    query = 'SELECT MIN(tobs), AVG(tobs), MAX(tobs) FROM measurement WHERE date >= :start and date <= :end'
+    start_end_stats = session.execute(query, {'start':start, 'end':end})
+    start_end_stats_dict_list = []
+    for min_temp, avg_temp, max_temp in start_end_stats:
+        start_end_stats_dict = {}
+        start_end_stats_dict['min_temp'] = min_temp
+        start_end_stats_dict['avg_temp'] = round(avg_temp,1)
+        start_end_stats_dict['max_temp'] = max_temp
+        start_end_stats_dict_list.append(start_end_stats_dict)
+    return jsonify(start_end_stats_dict_list)
 
 # running the code with debugger on
 if __name__ == '__main__':
     app.run(debug=True)
-
